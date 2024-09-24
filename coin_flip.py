4192d8f8ddcb27a2163573ab0801af8df134d0ce
pranav-jay26 <p_jayakumar@u.pacific.edu>
2024-09-23 17:54:03 -0700

    coin flip code

---
+++ coin_flip.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import random
+
+x = rand.randint(0, 1)
+
+def flip(int: x) -> str:
+    if x == 1:
+        return "Heads"
+    else:
+        return "Tails"
+
+print(flip(x))
